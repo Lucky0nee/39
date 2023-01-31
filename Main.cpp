3d4c535c81c8d34047038d3750797b955bb43a91
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-01-31 12:53:22 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 void coutArr(vector<string> arr) {
@@ -9,19 +8,26 @@ void coutArr(vector<string> arr) {
 	cout << "\n";
 }
 
-void sortArr(vector<string>& arr, vector<string> alphabet) { // "Їжаченко", "Атанасів", "Іларій", "Єремій", "Ґудзь", "Янгель", "Андрійчук"
-	vector<int> ArrIndex;
-	for (int i = 0; i < (int)arr.size(); i++) { // "Їжаченко"
+void sortArr(vector<string>& arr, vector<string> alphabet) {
+	vector<int> ArrIndex, ArrIndex2;
+	for (int i = 0; i < (int)arr.size(); i++) {
 		for (int alphIndex = 0; alphIndex < (int)alphabet.size(); alphIndex++) { // "Ї"
-			if (arr[i][0] == alphabet[alphIndex][0]) { // "Ї" == "Ї"
+			if (arr[i][0] == alphabet[alphIndex][0])
 				ArrIndex.push_back(alphIndex);
-			}
+
+			if (arr[i][2] == alphabet[alphIndex][0])
+				ArrIndex2.push_back(alphIndex);
 		}
 	}
-
 	for (int i = 0; i < (int)ArrIndex.size() - 1; i++) {
 		for (int j = i + 1; j < (int)ArrIndex.size(); j++) {
-			if (ArrIndex[i] >= ArrIndex[j]) {
+			if (ArrIndex[i] == ArrIndex[j]) {
+				if (ArrIndex2[i] < ArrIndex2[j]) {
+					swap(ArrIndex2[i], ArrIndex2[j]);
+					swap(arr[i], arr[j]);
+				}
+			}
+			if (ArrIndex[i] > ArrIndex[j]) {
 				swap(ArrIndex[i], ArrIndex[j]);
 				swap(arr[i], arr[j]);
 			}
@@ -35,7 +41,8 @@ int main() {
 		"А", "а", "Б", "б", "В", "в", "Г", "г", "Ґ", "ґ", "Д", "д", "Е", "е", "Є", "є", "Ж", "ж", "З", "з",
 		"И", "и", "І", "і", "Ї", "ї", "Й", "й", "К", "к", "Л", "л", "М", "м", "Н", "н", "О", "о", "П", "п",
 		"Р", "р", "С", "с", "Т", "т", "У", "у", "Ф", "ф", "Х", "х", "Ц", "ц", "Ч", "ч", "Ш", "ш", "Щ", "щ",
-		"ь", "Ю", "ю", "Я", "я" };
+		"ь", "Ю", "ю", "Я", "я"
+	};
 
 	vector<string> arr = { "Їжаченко", "Атанасів", "Іларій", "Єремій", "Ґудзь", "Янгель", "Андрійчук" };
 	cout << "Масив до сортування: "; coutArr(arr);
@@ -47,7 +54,6 @@ int main() {
 /*
 Написати програму, яка впорядковує українські прізвища за українською абеткою.
 Приклад вектора прізвищ для тестування:
-
 невпорядкований
 {Їжаченко, Атанасів, Іларій, Єремій, Ґудзь, Янгель, Андрійчук}
 впорядкований
